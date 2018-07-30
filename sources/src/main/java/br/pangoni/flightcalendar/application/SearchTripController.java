50f3c6a8ac2ace79f360b7796a1bf963d686907a
Marcos Augusto Pangoni <marcos@pangoni.com.br>
2018-07-30 08:05:24 -0300

    Some initial Classes and Web propotype

---
+++ sources/src/main/java/br/pangoni/flightcalendar/application/SearchTripController.java
@@ -0,0 +1,20 @@
+package br.pangoni.flightcalendar.application;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import br.pangoni.flightcalendar.domain.Trip;
+
+
+@RestController
+public class SearchTripController {
+
+    public List<Trip> queryTrips() {
+        return null;
+    }
+
+    public void selectTrip() {
+
+    }
+}
---
+++ sources/src/main/java/br/pangoni/flightcalendar/domain/Trip.java
@@ -0,0 +1,9 @@
+package br.pangoni.flightcalendar.domain;
+
+import java.io.Serializable;
+
+public class Trip implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
